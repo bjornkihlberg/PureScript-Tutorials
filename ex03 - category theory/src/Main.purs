c94be6bea2d93a215f00123005f42d1882992aae
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-11 16:16:48 +0200

    functor example code

--- ex03 - category theory/src/Main.purs
+++ ex03 - category theory/src/Main.purs
@@ -1,3 +1,11 @@
 module Main where
 
 import Prelude
+
+newtype Box a = Box a
+
+instance functorBox :: Functor Box where
+    map f (Box x) = Box (f x)
+
+instance showBox :: (Show a) => Show (Box a) where
+    show (Box x) = "Box " <> (show x)
