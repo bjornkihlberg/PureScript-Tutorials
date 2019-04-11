9f9a97a86b70914b13b440b9ad9829fe53a0f745
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-11 12:26:45 +0200

    use predicates instead to simplify implementation

--- ex01 - unit testing/test/Main.purs
+++ ex01 - unit testing/test/Main.purs
@@ -4,7 +4,6 @@ import Prelude
 
 import Effect (Effect)
 import Main (square)
-import Test.QuickCheck ((===), (>?))
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
@@ -15,8 +14,8 @@ squareTestSuite = suite "square" do
     test "square 0 = 0" $ Assert.equal (square 0.0) 0.0
     test "square 2 = 4" $ Assert.equal (square 2.0) 4.0
     test "square 3 = 9" $ Assert.equal (square 3.0) 9.0
-    test "square x = square -x" $ quickCheck (\ x -> square x === square (-x))
-    test "square x > 0" $ quickCheck (\ x -> square x >? 0.0)
+    test "square x = square -x" $ quickCheck (\ x -> square x == square (-x))
+    test "square x > 0" $ quickCheck (\ x -> square x > 0.0)
 
 main :: Effect Unit
 main = runTest squareTestSuite
