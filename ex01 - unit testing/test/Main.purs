e3b01ceff7cedac42829412aadafc8911b289300
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-11 11:51:12 +0200

    fix tests for square function

--- ex01 - unit testing/test/Main.purs
+++ ex01 - unit testing/test/Main.purs
@@ -2,17 +2,18 @@ module Test.Main where
 
 import Prelude
 
-import Main (f)
+import Main (square)
 
 import Effect (Effect)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 
-ftests :: TestSuite
-ftests = suite "ftests" do
-    test "f 0 = 0" $ Assert.equal (f 0) 0
-    test "f 2 = 4" $ Assert.equal (f 2) 4
+squareTests :: TestSuite
+squareTests = suite "square tests" do
+    test "square 0 = 0" $ Assert.equal (square 0) 0
+    test "square 2 = 4" $ Assert.equal (square 2) 4
+    test "square 3 = 9" $ Assert.equal (square 3) 9
 
 main :: Effect Unit
-main = runTest ftests
+main = runTest squareTests
