1a74eebd2658e642cb1a3af4aaa04a1b45902ae6
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-11 14:52:40 +0200

    some code to try out in repl

--- ex02 - repl/src/Main.purs
+++ ex02 - repl/src/Main.purs
@@ -1,3 +1,21 @@
 module Main where
 
 import Prelude
+
+f :: Number -> Number
+f x = x * 4.5
+
+newtype Box a = Box a
+
+g :: forall a. Box a -> a
+g (Box x) = x
+
+data Fruit = Orange | Banana
+
+instance showFruit :: Show Fruit where
+    show Orange = "orange"
+    show Banana = "banana"
+
+h :: Fruit -> String
+h Orange = "orange!"
+h Banana = "yellow!"
