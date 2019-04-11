19ea36e70621c7b0676fd9b137d4592abca5744f
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-11 12:18:35 +0200

    include property tests

--- ex01 - unit testing/test/Main.purs
+++ ex01 - unit testing/test/Main.purs
@@ -2,18 +2,21 @@ module Test.Main where
 
 import Prelude
 
-import Main (square)
-
 import Effect (Effect)
+import Main (square)
+import Test.QuickCheck ((===), (>?))
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
+import Test.Unit.QuickCheck (quickCheck)
 
-squareTests :: TestSuite
-squareTests = suite "square tests" do
-    test "square 0 = 0" $ Assert.equal (square 0) 0
-    test "square 2 = 4" $ Assert.equal (square 2) 4
-    test "square 3 = 9" $ Assert.equal (square 3) 9
+squareTestSuite :: TestSuite
+squareTestSuite = suite "square" do
+    test "square 0 = 0" $ Assert.equal (square 0.0) 0.0
+    test "square 2 = 4" $ Assert.equal (square 2.0) 4.0
+    test "square 3 = 9" $ Assert.equal (square 3.0) 9.0
+    test "square x = square -x" $ quickCheck (\ x -> square x === square (-x))
+    test "square x > 0" $ quickCheck (\ x -> square x >? 0.0)
 
 main :: Effect Unit
-main = runTest squareTests
+main = runTest squareTestSuite
