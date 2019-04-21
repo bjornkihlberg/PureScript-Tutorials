731fc8582374156a80b6859b63808a057813e787
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-21 20:26:17 +0200

    simplify custom lens

--- ex05 - optics/src/Main.purs
+++ ex05 - optics/src/Main.purs
@@ -7,9 +7,4 @@ import Data.Lens (Lens, lens)
 type MyRecord = { x :: Int, y :: Boolean, z :: String }
 
 _X :: Lens MyRecord MyRecord Int Int
-_X = lens getter setter
-    where
-        getter :: MyRecord -> Int
-        getter = _.x
-        setter :: MyRecord -> Int -> MyRecord
-        setter = _ { x = _ }
+_X = lens _.x $ _ { x = _ }
