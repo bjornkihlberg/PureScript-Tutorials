5d7be3f4613de0425879714492362086a7bbeecd
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-21 16:34:36 +0200

    handcrafted getter and setter

--- ex05 - optics/src/Main.purs
+++ ex05 - optics/src/Main.purs
@@ -1,3 +1,12 @@
 module Main where
-  
+
 import Prelude
+
+type RecordA = { b :: RecordB }
+type RecordB = { x :: Int }
+
+getX :: RecordA -> Int
+getX a = a.b.x
+
+setX :: Int -> RecordA -> RecordA
+setX x a = a { b = a.b { x = x } }
