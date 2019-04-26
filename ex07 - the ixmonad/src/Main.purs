ccee09be9c62c42e120aef851fd8745b79f3820e
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-26 10:06:57 +0200

    basic ixmonad example

--- ex07 - the ixmonad/src/Main.purs
+++ ex07 - the ixmonad/src/Main.purs
@@ -1,3 +1,43 @@
 module Main where
 
 import Prelude
+
+import Control.Monad.Indexed (class IxApplicative, class IxApply, class IxBind, class IxFunctor, class IxMonad, (:>>=))
+import Data.Newtype (class Newtype, unwrap)
+
+newtype IxABC i o a = IxABC a
+
+derive instance newtypeIxABC :: Newtype (IxABC i o a) _
+
+instance showABC :: (Show a) => Show (IxABC i o a) where
+    show (IxABC x) = "IxABC (" <> show x <> ")"
+
+instance ixFunctorABC :: IxFunctor IxABC where
+    imap f (IxABC x) = IxABC (f x)
+
+instance ixApplyABC :: IxApply IxABC where
+    iapply (IxABC f) (IxABC x) = IxABC (f x)
+
+instance ixApplicativeABC :: IxApplicative IxABC where
+    ipure = IxABC
+
+instance ixBindABC :: IxBind IxABC where
+    ibind (IxABC x) f = (IxABC <<< unwrap) (f x)
+
+instance ixMonadABC :: IxMonad IxABC
+
+data A
+data B
+data C
+
+doA :: Int -> IxABC Void A Int
+doA = IxABC
+
+doB :: Int -> IxABC A B Int
+doB = (_ * 2) >>> IxABC
+
+doC :: Int-> IxABC B C Int
+doC = (_ - 1) >>> IxABC
+
+myABC :: IxABC Void C Int
+myABC = doA 5 :>>= doB :>>= doC
