bd9451947bfabca25b3644b1aae9c19ca6bb94d5
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-27 23:00:14 +0200

    example implementation

--- ex08 - state monad/src/Main.purs
+++ ex08 - state monad/src/Main.purs
@@ -1,3 +1,20 @@
 module Main where
 
 import Prelude
+
+import Control.Monad.State (State, state)
+import Data.Int.Bits (shr, (.&.))
+import Data.Tuple (Tuple(..))
+
+fastRand :: Int -> Int
+fastRand seed = (214013 * seed + 2531011) `shr` 16 .&. 5
+
+fastRandSt :: State Int Int
+fastRandSt = state (\seed -> let r = fastRand seed in Tuple r r)
+
+randomSum :: State Int Int
+randomSum = do
+    a <- fastRandSt
+    b <- fastRandSt
+    c <- fastRandSt
+    pure (a + b + c)
