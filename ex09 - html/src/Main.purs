bb3b0307ea655b14e61d51d3d54f5e1c26a0a189
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-04-29 12:36:50 +0200

    attaching event handler to button

--- ex09 - html/src/Main.purs
+++ ex09 - html/src/Main.purs
@@ -2,9 +2,29 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Console (log)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.HTML (window)
-import Web.HTML.Window (alert)
+import Web.HTML.Event.EventTypes (click)
+import Web.HTML.HTMLButtonElement (fromElement, toEventTarget)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
 main :: Effect Unit
-main = window >>= alert "Hello, world!"
+main = do
+    w <- window
+    d <- document w
+    let myButtonNode = toNonElementParentNode d
+    maybeMyButtonElement <- getElementById "mybutton" myButtonNode
+    case maybeMyButtonElement of
+        Nothing -> log "could not find element by given id"
+        Just myButtonElement -> 
+            case fromElement myButtonElement of
+                Nothing -> log "given element was not a button"
+                Just myButton -> do
+                    let myButtonEventTarget = toEventTarget myButton
+                    myButtonClickEvent <- eventListener (\_ -> log "You clicked!")
+                    addEventListener click myButtonClickEvent false myButtonEventTarget
