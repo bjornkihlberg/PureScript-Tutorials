1bc1fb8766fc2ed3f1b0b94196e84fa4963b60a8
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-05-03 20:30:42 +0200

    minimal buggy example

--- ex11 - behaviors/src/Main.purs
+++ ex11 - behaviors/src/Main.purs
@@ -1,7 +1,15 @@
 module Main where
 
 import Prelude
+
+import Data.Maybe (maybe)
 import Effect (Effect)
+import Effect.Console (log)
+import FRP.Behavior (animate)
+import FRP.Behavior.Mouse (position)
+import FRP.Event.Mouse (getMouse)
 
 main :: Effect Unit
-main = pure unit
+main = do
+    mousePos <- getMouse <#> position
+    animate mousePos (maybe (pure unit) (show >>> log)) # join
